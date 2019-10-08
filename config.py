3d6a5da0ebd392c5979b50f4c9836bfaf47ad972
JOFLIX <joflixooko@outlook.com>
2019-10-08 13:14:14 +0300

    deployment

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+# This configures our application to behave as required
+
+class Config:
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # Defines the key required by the flask forms
+    SECRET_KEY =    '1234'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/hudson'
+
+
+    # Defines where the users profile pictures will be stored
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # Defines email configurations that will be used to send emails to the registered users
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "oliveenterprise2@gmail.com"
+    MAIL_PASSWORD = "NOTEKCS158M"
+
+    def init_app(app):
+        pass
+
+
+# This defines the configurations during production of the application
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/hudson'
+
+
+
+    pass
+
+class DevConfig(Config):
+
+
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
